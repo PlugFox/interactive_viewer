0fb78989951703323a418a63854f32328f4ead30
Plague Fox <plugfox@gmail.com>
2021-07-01 02:39:38 +0300

    Новый механизм отрисовки доски

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,18 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:game_board/src/board.dart';
-import 'package:game_board/src/camera.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => const MaterialApp(
-        title: 'GameBoard',
+  Widget build(BuildContext context) => MaterialApp(
+        title: 'Board',
         home: Scaffold(
           body: SafeArea(
-            child: Camera(
-              debug: true,
-              child: Board(),
+            child: Board(
+              builder: (x, y) => DecoratedBox(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    width: 1,
+                    color: const Color(0xFF000000),
+                  ),
+                ),
+                child: Center(
+                  child: Text('$x x $y'),
+                ),
+              ),
             ),
           ),
         ),
--- lib/src/board.dart
+++ lib/src/board.dart
@@ -1,64 +1,176 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 
+typedef CellBuilder = Widget Function(int x, int y);
+
+@immutable
 class Board extends StatelessWidget {
+  final Size? size;
+  final CellBuilder builder;
+
   const Board({
+    required this.builder,
+    this.size,
     Key? key,
   }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => Column(
-        children: List<Widget>.generate(
-          8,
-          (i) => Expanded(
-            flex: 1,
-            child: Row(
-              children: List<Widget>.generate(
-                8,
-                (j) => Expanded(
-                  flex: 1,
-                  child: BoardCell(
-                    x: j,
-                    y: i,
-                  ),
+  Widget build(BuildContext context) => _Board(
+        builder: builder,
+        size: size ?? const Size.square(100),
+      );
+}
+
+@immutable
+class _Board extends StatefulWidget {
+  final Size size;
+  final CellBuilder builder;
+
+  const _Board({
+    required this.builder,
+    required this.size,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<_Board> createState() => _BoardState();
+}
+
+// ignore: prefer_mixin
+class _BoardState extends State<_Board> {
+  final ValueNotifier<Offset> _listenable =
+      ValueNotifier<Offset>(const Offset(0, 0));
+
+  //region Lifecycle
+  @override
+  void initState() {
+    super.initState();
+    // Первичная инициализация виджета
+  }
+
+  @override
+  void didUpdateWidget(_Board oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    // Конфигурация виджета изменилась
+  }
+
+  @override
+  void dispose() {
+    // Перманетное удаление стейта из дерева
+    super.dispose();
+  }
+  //endregion
+
+  Iterable<Widget> _buildCells(int width, int height) sync* {
+    final builder = widget.builder;
+    final cellSize = widget.size;
+    for (var x = 0; x < width; x++) {
+      for (var y = 0; y < height; y++) {
+        yield SizedBox.fromSize(
+          size: cellSize,
+          child: builder(x, y),
+        );
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) => GestureDetector(
+        onPanStart: (DragStartDetails details) {},
+        onPanCancel: () {},
+        onPanDown: (details) {},
+        onPanEnd: (details) {},
+        onPanUpdate: (details) {
+          _listenable.value =
+              _listenable.value.translate(details.delta.dx, details.delta.dy);
+        },
+        child: ColoredBox(
+          color: const Color(0xFF7F7F7F),
+          child: LayoutBuilder(
+            builder: (context, constraints) {
+              // Расчитываю сколько клеточек может
+              // поместиться на экране по каждой оси
+              // с небольшим запасом
+              final boardSize = constraints.biggest;
+              final cellSize = widget.size;
+              final width = (boardSize.width / cellSize.width).ceil() + 1;
+              final height = (boardSize.height / cellSize.height).ceil() + 1;
+              return Flow(
+                delegate: _BoardFlowDelegate(
+                  width,
+                  height,
+                  cellSize,
+                  _listenable,
                 ),
-              ),
-            ),
+                children: _buildCells(width, height).toList(growable: false),
+              );
+            },
           ),
         ),
       );
 }
 
-@immutable
-class BoardCell extends StatelessWidget {
-  static const Color _black = Color(0xFF000000);
-  static const Color _white = Color(0xFFFFFFFF);
-  static final int _aCode = 'A'.codeUnitAt(0);
+class _BoardFlowDelegate extends FlowDelegate {
+  final ValueListenable<Offset> listenable;
+  final int width;
+  final int height;
+  final Size size;
 
-  final int x;
-  final int y;
-
-  const BoardCell({
-    required this.x,
-    required this.y,
-    Key? key,
-  }) : super(key: key);
+  _BoardFlowDelegate(
+    this.width,
+    this.height,
+    this.size,
+    this.listenable,
+  ) : super(repaint: listenable);
 
   @override
-  Widget build(BuildContext context) {
-    final whiteCell = x.isOdd ^ y.isOdd;
-    return ColoredBox(
-      color: whiteCell ? _white : _black,
-      child: Center(
-        child: Text(
-          '${String.fromCharCode(x + _aCode)}${y + 1}',
-          textAlign: TextAlign.center,
-          maxLines: 1,
-          style: TextStyle(
-            color: whiteCell ? _black : _white,
-            fontSize: 48,
+  void paintChildren(FlowPaintingContext context) {
+    // Количество целых столбцов и строк на которые съехала доска
+    // по горизонтали и вертикали
+    final colOffset = -(listenable.value.dx / size.width).ceil();
+    final rowOffset = -(listenable.value.dy / size.height).ceil();
+
+    // Отступы для смещения в количестве досок
+    var xBoardOffset = 0;
+    var yBoardOffset = 0;
+
+    var i = 0;
+    for (var x = 0; x < width; x++) {
+      // Перемещение столбца
+      if (!colOffset.isNegative) {
+        // Столбцы уходят влево - перемещаем первые столбцы справа от остальных
+        /// TODO: вывести формулу на сколько досок нужно отступить клетке влево или вправо
+        //if (x < colOffset.abs()) {
+        //  xBoardOffset = (colOffset ~/ width + 1) * width;
+        //} else {
+        //  xBoardOffset = 0;
+        //}
+      } else {
+        // // Столбцы уходят вправо - перемещаем правые столбцы слева от остальных
+        // if (x > colOffset.abs()) {
+        //   xOffset = -width;
+        // } else {
+        //   xOffset = 0;
+        // }
+      }
+      for (var y = 0; y < height; y++) {
+        context.paintChild(
+          i++,
+          opacity: 1,
+          transform: Matrix4.translationValues(
+            (x + xBoardOffset) * size.width + listenable.value.dx,
+            (y + yBoardOffset) * size.height + listenable.value.dy,
+            0,
           ),
-        ),
-      ),
-    );
+        );
+      }
+    }
   }
+
+  @override
+  bool shouldRepaint(covariant _BoardFlowDelegate oldDelegate) =>
+      !identical(listenable, oldDelegate.listenable) ||
+      width != oldDelegate.width ||
+      height != oldDelegate.height ||
+      size != oldDelegate.size;
 }
